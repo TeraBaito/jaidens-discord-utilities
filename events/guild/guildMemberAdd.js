8bfed9d9d5d5e29adf7fd2cd122e98f2107ae959
TeraBaito <lrobles2670@gmail.com>
2020-09-20 18:16:03 -0600

    1.3.0 Added guildMemberRemove event

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -6,6 +6,6 @@
 const Discord = require('discord.js');
 
 module.exports = async (bot, member, message) => {
-    // Just sends a cool message in chat to welcome the user (unless it's a bot bc :moyai:)
-    if (!message.author.bot) bot.channels.get('755182878635327529').send(`Hello, ${member.id}. Welcome to r/JaidenAnimations!`);
+    // Just sends a cool message in chat to welcome the user
+    bot.channels.cache.get('755182878635327529').send(`Hello, <@${member.id}>. Welcome to r/JaidenAnimations!`);
 };
---
+++ events/guild/guildMemberRemove.js
@@ -0,0 +1,11 @@
+/**
+ * `guildMemberRemove` event
+ * Emitted whenever a user leaves a guild
+ */
+
+const Discord = require('discord.js');
+
+module.exports = async (bot, member, message) => {
+    // ok cya
+    bot.channels.cache.get('755182878635327529').send(`Welps, guess like <@${member.id}> couldn't stand to be around us, adiós.`);
+};
