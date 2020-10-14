c4c707b2577db5302492121770e6b597f1cb29f1
TeraBaito <lrobles2670@gmail.com>
2020-10-13 19:15:48 -0600

    Used bold text

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -7,5 +7,5 @@ const Discord = require('discord.js');
 
 module.exports = async (bot, member, message) => {
     // ok cya
-    bot.channels.cache.get('755182878635327529').send(`Welps, guess like ${member.displayName} couldn't stand to be around us, adiós.`);
+    bot.channels.cache.get('755182878635327529').send(`Welps, guess like **${member.displayName}** couldn't stand to be around us, adiós.`);
 };
