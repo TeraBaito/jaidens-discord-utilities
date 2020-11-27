73a00e34787cf1ed3ea167ade73d28d18ef85356
MaxTechnics <58440941+MaxTechnics@users.noreply.github.com>
2020-11-27 17:58:38 +0100

    Update grady.js
    He wants to be pinged for some reason

--- commands/Miscellaneous/grady.js
+++ commands/Miscellaneous/grady.js
@@ -10,6 +10,6 @@ module.exports = {
 
     run: async(bot, message, args) => {
         if (message.deletable) message.delete();
-        message.channel.send('Acama (Grady), is taking a break from Discord. However, he will return.');
+        message.channel.send('<@740491200972193793> (Grady), is taking a break from Discord. However, he will return.');
     }
 };
