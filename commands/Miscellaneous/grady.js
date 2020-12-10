6b6ed4f1c4dfa99bb9116097b71b898b28b26054
MaxTechnics <58440941+MaxTechnics@users.noreply.github.com>
2020-12-10 17:21:44 +0100

    Grady's back
    Yes

--- commands/Miscellaneous/grady.js
+++ commands/Miscellaneous/grady.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'grady',
-    helpName: 'Grady gone...',
+    helpName: 'Grady was gone...',
     category: 'Miscellaneous',
     aliases: ['acama'],
     usage: 'grady',
-    description: 'He will come back.',
+    description: 'The Great Grady has returned.',
 
     run: async(bot, message, args) => {
         if (message.deletable) message.delete();
-        message.channel.send('<@740491200972193793> (Grady), is taking a break from Discord. However, he will return.');
+        message.channel.send('Grady is back, there\'s no need to use this command anymore.');
     }
 };
