70f7a62e425e3c1629463e41916ec50f307027cd
TeraBaito <lrobles2670@gmail.com>
2021-01-18 20:15:07 -0600

    1.11.0 Added extra info in guildMemberAdd message so people read rules kekw

--- src/events/guild/guildMemberAdd.js
+++ src/events/guild/guildMemberAdd.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { stripIndents } = require('common-tags');
 
 /**
  * `guildMemberAdd` event.
@@ -10,5 +11,8 @@ const Discord = require('discord.js');
 module.exports = async (bot, member) => {
     // Just sends a cool message in chat to welcome the user
     if (member.guild.id != '754451472699228281') return;
-    bot.channels.cache.get('755182878635327529').send(`Hello, <@${member.id}>. Welcome to r/JaidenAnimations!`);
+    bot.channels.cache.get('755182878635327529').send(
+        stripIndents`Hello, <@${member.id}>. Welcome to r/JaidenAnimations!
+    
+    Please make sure to read <#755180458563600445> and the pinned comments / topics for this and other channels`);
 };
