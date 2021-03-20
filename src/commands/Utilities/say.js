44d1371813358f7ec887196275d3a3b24699e229
TeraBaito <lrobles2670@gmail.com>
2021-03-20 09:56:42 -0600

    1.9.1 Reverted funi pent troll

--- src/commands/Utilities/say.js
+++ src/commands/Utilities/say.js
@@ -18,10 +18,8 @@ module.exports = {
         let channel = message.mentions.channels.first() ||
         message.guild.channels.cache.find(c => c.id == args[0]) ||
         message.guild.channels.cache.find(c => c.name == args[0]);
-	    
-	if (message.author.id == '725836730846019644') return message.channel.send('get trolled <:troll:798638733095075875>');
-        
-	if (!channel) return message.channel.send('Couldn\'t find a channel with the arguments provided');
+	            
+        if (!channel) return message.channel.send('Couldn\'t find a channel with the arguments provided');
     
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return;
 
