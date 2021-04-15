f1bbaab13495f054989ca61c499340b65de78dfd
TeraBaito <lrobles2670@gmail.com>
2021-04-15 09:28:06 -0600

    Added a command

---
+++ src/commands/Fun/a.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'a',
+    helpName: 'a.',
+    category: 'Fun',
+    aliases: ['shrimps-favorite-command'],
+    usage: 'a',
+    description: 'Shrimp\'s favorite command',
+
+    /**
+    * @param {Discord.Client} bot
+    * @param {Discord.Message} message
+    * @param {Array} args
+    */
+    run: async(bot, message, args) => {
+        if (!['558264504736153600', '291684752363225098', '488064501816492047']
+            .includes(message.author.id)) 
+            
+            return message.channel.send('lmao no, this command is only shrimp irony idot. good luck next time or not');
+
+        if (message.deletable) message.delete();
+        message.channel.send('a.');
+    }
+};
