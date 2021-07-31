c825f0f41adf5d1e7f9844df09d16b69484390ba
TeraBaito <lrobles2670@gmail.com>
2021-07-31 10:53:22 -0600

    3.4.0 Instead of sending the welcome message when the user joins send it when the Member role is added
    sorry for anyone who wants to copy this code but doesn't need that capability

--- src/events/guild/guildMemberAdd.js
+++ src/events/guild/guildMemberAdd.js
@@ -1,11 +1,9 @@
 const { MessageEmbed, GuildMember } = require('discord.js');
 const Bot = require('../../../Bot');
-const { stripIndents } = require('common-tags');
-const { readJSONSync } = require('fs-extra');
 const { formatDate, unhoistOne } = require('../../handlers/functions');
-const { jaidenServerID, mainChannel } = require('../../../config.json');
+const { jaidenServerID } = require('../../../config.json');
 const colors = require('../../../colors.json');
-const eggs = require('../../handlers/eastereggs.json');
+
 
 /**
  * `guildMemberAdd` event.
@@ -16,8 +14,7 @@ const eggs = require('../../handlers/eastereggs.json');
  * @param {GuildMember} member 
  */
 module.exports = async (bot, member) => {
-    const { welcomer } = readJSONSync('./botSettings.json');
-    if (!welcomer || member.guild.id != jaidenServerID) return;
+    if (member.guild.id != jaidenServerID) return;
     
     const embeds = [ new MessageEmbed()
         .setColor(colors.Olive)
@@ -27,20 +24,6 @@ module.exports = async (bot, member) => {
         .addField('Joined Server', formatDate(member.joinedAt))
         .addField('Joined Discord', formatDate(member.user.createdAt))
         .setThumbnail(member.user.displayAvatarURL({ format: 'png', dynamic: true })) ];
-
-    // 0 // 0 < 11 = true // eggs[0]
-    // 11 // 11 < 11 = false // default
-    // 10 // 10 < 11 = true // eggs[10]
-
-    const num = Math.floor(Math.random() * 100);
-    const msg = num < eggs.length ?
-        `<@${member.id}> ${eggs[num]}` :
-        stripIndents`Hello, <@${member.id}>. Welcome to r/JaidenAnimations!
-    Please make sure to read <#755180458563600445> and the pinned comments / topics for this and other channels.
-    And for the context, Jaiden isn't here :p`;
-
-    // Just sends a cool message in chat to welcome the user
-    bot.channels.cache.get(mainChannel).send(msg);
 
     bot.guilds.cache.get(jaidenServerID).channels.cache.find(ch => ch.name == 'new-members').send({ embeds });
     unhoistOne(member);
--- src/events/guild/guildMemberUpdate.js
+++ src/events/guild/guildMemberUpdate.js
@@ -1,6 +1,10 @@
 const { GuildMember } = require('discord.js');
+const { readJSONSync } = require('fs-extra');
+const { stripIndents } = require('common-tags');
 const Bot = require('../../../Bot');
 const { unhoistOne } = require('../../handlers/functions');
+const { memberRole, jaidenServerID, mainChannel } = require('../../../config.json');
+const eggs = require('../../handlers/eastereggs.json');
 
 /**
  * `guildMemberUpdate` event.
@@ -13,5 +17,29 @@ const { unhoistOne } = require('../../handlers/functions');
  * @param {GuildMember} newMember 
  */
 module.exports = async (bot, oldMember, newMember) => {
-    unhoistOne(newMember);
+    if (newMember.guild.id !== jaidenServerID) return;
+
+    if (oldMember.partial) oldMember.fetch();
+    if (newMember.partial) newMember.fetch();
+
+    if (oldMember.nickname === newMember.nickname) unhoistOne(newMember);
+
+    if (newMember.roles.cache.get(memberRole)) {
+        const { welcomer } = readJSONSync('./botSettings.json');
+        if (!welcomer) return;
+
+        // 0 // 0 < 11 = true // eggs[0]
+        // 11 // 11 < 11 = false // default
+        // 10 // 10 < 11 = true // eggs[10]
+
+        const num = Math.floor(Math.random() * 100);
+        const msg = num < eggs.length ?
+            `<@${newMember.id}> ${eggs[num]}` :
+            stripIndents`Hello, <@${newMember.id}>. Welcome to r/JaidenAnimations!
+    Please make sure to read <#755180458563600445> and the pinned comments / topics for this and other channels.
+    And for the context, Jaiden isn't here :p`;
+
+        // Just sends a cool message in chat to welcome the user
+        bot.channels.cache.get(mainChannel).send(msg);
+    }
 };
