5dd6aeabaa2c95900c4d2ac303ec326a74e45178
TeraBaito <lrobles2670@gmail.com>
2021-09-23 12:52:14 -0600

    3.6.4 Better, fixed pagination for listtags

--- src/commands/Tags/listtags.js
+++ src/commands/Tags/listtags.js
@@ -28,32 +28,33 @@ module.exports = {
                 return 0;
             })
             .map(t => bold(t.name) + ' - by ' + t.username);
-        
-        // Find out how many splits it'll need (it'll split every closest number >=200 characters)
-        // I made it using the tags as string[] so it wouldn't split between a desc
-        // NOTE: Splits are actually EVERY HOW MUCH it'll flatten, not HOW MUCH TIMES it will
-        // e.g.: 2 splits will flatten every 2 tags, arr.length splits will not flatten
-        let curChars = 0, splits = 0;
-        tags.forEach(tag => {
-            curChars += tag.length;
-            if (curChars >= 200) { curChars = 0; splits++; }
-        });
 
-        // i = 0 | tags = 6 | splits = 3 => push 0, 0+3
-        // i = 3 | push 3, 3+3
-        let split = [];
-        if (splits > 0) for (let i = 0; i < tags.length; i += splits) split.push(tags.slice(i, i + splits).join('\n'));
-        else split = [tags.join('\n')];
+        /**
+         * Splits an array every x elements
+         * from stackoverflow lolllllll
+         * @param {Array} arr 
+         * @param {number} size Every how much it'll split
+         * @returns {Array}
+         */
+        function paginate (arr, size) {
+            return arr.reduce((acc, val, i) => {
+                let idx = Math.floor(i / size);
+                let page = acc[idx] || (acc[idx] = []);
+                page.push(val);
+              
+                return acc;
+            }, []);
+        }
 
         // Current page (args[0] or 0), if it's less it returns
-        let curPage = parseInt(args[0]) - 1 || 0;
+        let curPage = parseInt(args[0]) - 1|| 0;
         if (curPage < 0) return message.channel.send('Please put a valid page number!');
-
+        let pages = paginate(tags, 10);
         let embeds = [
             new MessageEmbed()
                 .setTitle('Tags List')
-                .setDescription(split[curPage])
-                .setFooter(`Page ${curPage+1} of ${split.length}`)
+                .setDescription(pages[curPage].join('\n'))
+                .setFooter(`Page ${curPage+1} of ${pages.length}`)
                 .setColor(SteelBlue)
         ];
 
